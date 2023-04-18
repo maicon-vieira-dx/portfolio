e6dbee90a0f1978572946afd01464aecea05b942
maicon-vieira-dx <maicon10042001@gmail.com>
2023-04-18 16:59:11 -0300

    Alterando nome

--- src/mocks/profile.js
+++ src/mocks/profile.js
@@ -147,7 +147,7 @@ const profile = {
                     Icon: vue,
                 },
                 {
-                    Name: "atom",
+                    Name: "react",
                     Icon: atom,
                 }, 
                 {
